5b59898095cdaf87c8be579a012f5f2d32972db3
daviferrreira0-droid <daviferrreira0@gmail.com>
2026-06-11 07:03:42 -0300

    Create Tela-Principal.py
    primeiras classes

---
+++ Tela-Principal.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+import random as rr
+from tkinter import messagebox
+'''aqui colocar a parte funções'''
+class Pessoa():
+    def __init__(self,nome):
+        self.nome=nome
+        self.idade=0
+class Dsexo(Pessoa):
+   listas = ['masculino','feminino']
+   sexo = rr.choice(listas)
+class Riqueza(Pessoa):
+    li = ['Rico','Medio','Pobre']
+    csocial = rr.choice(li)
+class Edu(Pessoa):
+    pass  
+class Prof(Pessoa):
+    pass 
+class saude(Pessoa):
+    pass
+
+class PriEscolha():
+    opcao=[]
+    messagebox.showinfo('Escolha',f'Você escolheu:{opcao}')
+    def criar_interface():
+        janela = tk.Tk()
+        janela.title("Ano 0")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''Aqui colocar a interface grafica'''
+class Tela_Principal(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.tela= tk.Label(text='Menu')
+        self.tela.grid(row=2,column=1)
+
+        self.but1=tk.Button(text='iniciar jogo')
+        self.but1.grid(row=2,column=2)
+
+
+pri= Tela_Principal()
+pri.geometry("700x500")
+pri.mainloop
